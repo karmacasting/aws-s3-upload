9272b0d7ecb3605ae67f30598db6460ba5058678
Julian Paas <jpaas@users.noreply.github.com>
2023-07-28 16:31:42 -0400

    First attempt at actual upload

--- src/s3Upload.ts
+++ src/s3Upload.ts
@@ -1,15 +1,29 @@
-import { getInput } from "@actions/core"
+import { getInput, setCommandEcho } from "@actions/core"
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import fs from "fs"
 
 const githubInputs = () => {
   return {
     bucket: getInput("bucket", { required: true }),
     file: getInput("file", { required: true }),
     key: getInput("key", { required: true }),
-    region: getInput("region", { required: true }),
   }
 }
 
 export const runBuild = async (): Promise<string | void> => {
-  console.log(githubInputs())
-  return
+  setCommandEcho(true)
+  const inputs = githubInputs()
+  console.log(inputs)
+  const s3Client = new S3Client({})
+  try {
+    const fileContent = fs.readFileSync(inputs.file, "utf-8")
+    const result = await s3Client.send(
+      new PutObjectCommand({ Body: fileContent, Bucket: inputs.bucket, Key: inputs.key }),
+    )
+    console.log(result)
+    return
+  } catch (error) {
+    console.log(error)
+    return error instanceof Error ? error.message : "Unknown error"
+  }
 }
