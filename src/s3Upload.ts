486d40601fdb0d44452168192183e8c90662666d
Julian Paas <jpaas@users.noreply.github.com>
2023-08-30 11:16:43 -0400

    don't impose utf-8 as encoding

--- src/s3Upload.ts
+++ src/s3Upload.ts
@@ -15,7 +15,7 @@ export const runBuild = async (): Promise<string | void> => {
   console.log(inputs)
   const s3Client = new S3Client({})
   try {
-    const fileContent = fs.readFileSync(inputs.file, "utf-8")
+    const fileContent = fs.readFileSync(inputs.file)
     const result = await s3Client.send(
       new PutObjectCommand({ Body: fileContent, Bucket: inputs.bucket, Key: inputs.key }),
     )
